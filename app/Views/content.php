a40e21e180e986741af84e4090cb15d7217e66ef
ridintek <rw056h@gmail.com>
2023-01-22 14:34:52 +0700

    Update Chart.JS.

--- app/Views/content.php
+++ app/Views/content.php
@@ -9,7 +9,6 @@
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="<?= base_url() ?>/assets/modules/fontawesome/css/all.min.css">
   <!-- Third party -->
-  <link rel="stylesheet" href="<?= base_url() ?>/assets/modules/chart.js/Chart.min.css">
   <link rel="stylesheet" href="<?= base_url() ?>/assets/modules/datatables/datatables.min.css">
   <link rel="stylesheet" href="<?= base_url() ?>/assets/modules/fontawesome/css/all.min.css">
   <link rel="stylesheet" href="<?= base_url() ?>/assets/modules/flag-icon/css/flag-icon.min.css">
@@ -716,7 +715,7 @@
   <!-- Application -->
   <script src="<?= base_url() ?>/assets/modules/bootstrap-validate/bootstrap-validate.js"></script>
   <script src="<?= base_url() ?>/assets/modules/bs-custom-file-input/bs-custom-file-input.min.js"></script>
-  <script src="<?= base_url() ?>/assets/modules/chart.js/Chart.min.js"></script>
+  <script src="<?= base_url() ?>/assets/modules/chart.js/chart.umd.js"></script>
   <script src="<?= base_url() ?>/assets/modules/datatables/datatables.min.js"></script>
   <script src="<?= base_url() ?>/assets/modules/fullcalendar/lib/main.min.js"></script>
   <script src="<?= base_url() ?>/assets/modules/icheck/icheck.min.js"></script>
